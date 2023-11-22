e2fee0635764b4955c2a22466dc29344e79a837b
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-11-22 18:45:41 +0000

    working on menu

---
+++ src/Menu.js
@@ -0,0 +1,16 @@
+function Menu() {
+    return (
+      <div className="Menu">
+       <nav>
+        <ul>
+          <li>Fotos</li>
+          <li>Sugerencias</li>
+          <li>Nosotros</li>
+        </ul>
+      </nav>
+      </div>
+    );
+  }
+  
+  export default Menu;
+  
