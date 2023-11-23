06897286c00b10902c75e7effcf173b48b661d93
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-11-23 02:41:27 +0000

    doing menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Menu from './Menu';
+import image from './Image/NúnkaMagazine.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+     <div className='foto-logo'>
+      <img src={image} alt="Logo" />
+    </div>
         <Menu />
       </header>
     </div>
+  
   );
 }
 
