cbe74fb1b447b22d73189fc0e7755fb13d1af744
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-01-10 18:32:59 +0000

    add

--- src/fotos.js
+++ src/fotos.js
@@ -1,63 +1,29 @@
 import React from "react";
-import { Menu } from "./Menu";
-import { Search } from "./Search";
-import { Fotosviajes } from "./fotosviajes";
 
-const misViajesTodos = [
-  { text: 'Machu', completed:true},
-  { text: 'Paris', completed:false},
-  { text: 'Lima', completed:true},
-  { text: 'Paris', completed:false},
-  { text: 'Ir al gym', completed:true},
-  
-  ];
-
-function Fotos() {
-  const [todos, setTodos] = React.useState(misViajesTodos);
-  const [searchValue, setSearchValue] = React.
-useState('');
-
-const completedTodos = todos.filter(todo => !!todo.completed).length;
-const totalTodos = todos.length;
-
-const searchedTodos = todos.filter(
-  (todo) => {
-  
-    const todoText = todo.text.toLocaleLowerCase();
-    const searchText = searchValue.toLowerCase();
-   return  todoText.includes
-   (searchText);
-  }
-  
-  );
-
-  console.log('Usuarios made the day ' + searchValue)
+function Fotos( {
+  searchValue,
+  setSearchValue,
+  }) {
 
 
+    return(
+
+    <form>
+    <input
+     placeholder="Busca tu viaje"
+          className='TodoSearch'
+          value={searchValue}
+         onChange={(event) => {
+            setSearchValue(event.target.value);
+           
+
+          } 
+        }
+    />
+     </form>
+    );
+    }
+   
 
 
-
-  return (
-    <>
-      <Menu />
-      <Search 
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}/>
-  
-      <Fotosviajes>
-        {searchedTodos.map(todo => ( 
-          <TodoDay 
-          key={todo.text} 
-          text={todo.text}
-          completed={todo.completed}/>
-          
-          ))}
-      
-      </Fotosviajes>
-
-      <p>Ultimas fotos</p>
-    </>
-  );
-}
-
-export { Fotos };
+export {Fotos}
