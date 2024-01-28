7842633d8acb7a3397ef15edbc1be82069f19999
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-01-28 01:38:47 +0000

    fixing menu and add new

--- src/Menu.js
+++ src/Menu.js
@@ -2,36 +2,26 @@ import React from "react";
 import './App.css';
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function Menu() {
   return (
-    <div className="Menu">
-      <nav className="navbar navbar-expand-md navbar-light">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbar-toggler"
-            aria-controls="navbarTogglerDemo01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbar-toggler">
-            <ul className="navbar-nav d-flex justify-content-center align-items-center">
-              <li><Link to="/fotos">Fotos</Link></li>
-              <li><Link to="/blog">Blog</Link></li>
-              <li><Link to="/nosotros">Nosotros</Link></li>
-            </ul>
-          </div>
+    <nav className="navbar navbar-expand-md navbar-light">
+      <div className="container-fluid">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-toggler" aria-controls="navbar-toggler" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbar-toggler">
+          <a className="navbar-brand" href="#">Hidden brand</a>
+          <ul className="navbar-nav d-flex justify-content-center align-items-center">
+              <li className="nav-item"><Link to="/blog">Blog</Link></li>
+              <li className="nav-item"><Link to="/fotos">Fotos</Link></li>
+              <li className="nav-item"><Link to="/nosotros">Nosotros</Link></li>
+          </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
 
 export { Menu };
-
-  
--- src/about.js
+++ src/about.js
@@ -3,7 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function About() {
     return (
-      <div className="about d-flex  justify-content-center  align-items-center">
+
+      <section className="hero align-items-stretch">
+      <div className=" justify-content-center  align-items-center">
         <h1>
     <span className="d-xl-block">We are</span> <br></br>
     <span className="d-xl-block">all citizens</span> <br></br>
@@ -12,6 +14,7 @@ function About() {
 
         
       </div>
+      </section>
     
     );
   }
