1861d78a3c12f3959f8d731f460165c5f7341efd
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-04-23 01:37:00 +0100

    almost

--- src/data/blogpost.js
+++ src/data/blogpost.js
@@ -3,30 +3,37 @@ export const Blogs = [
     id:'1',
     img:'https://m.media-amazon.com/images/I/81aDxpFVw8L._AC_UF1000,1000_QL80_.jpg',
     Title:'Nunka',
-    Description:'Machu pichu es una de las maraviilas del mundo por el cual mucha gente se atrae y la pregunta como llegar si mi prespuesto es ajustado'
+    Description:'Nunka es una website hippie sobre viajes y experiencias atraves de esta website'
 
 },
 {
     id:'1',
-    img:'https://m.media-amazon.com/images/I/81aDxpFVw8L._AC_UF1000,1000_QL80_.jpg',
-    Title:'Nunka',
-    Description:'Machu pichu es una de las maraviilas del mundo por el cual mucha gente se atrae y la pregunta como llegar si mi prespuesto es ajustado'
+    img:'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Collage_Cuenca.png/800px-Collage_Cuenca.png',
+    Title:'Cuenca',
+    Description:'De las mejores ciudades del ecuador'
 
 },
 {
     id:'1',
-    img:'https://m.media-amazon.com/images/I/81aDxpFVw8L._AC_UF1000,1000_QL80_.jpg',
-    Title:'Nunka',
-    Description:'Machu pichu es una de las maraviilas del mundo por el cual mucha gente se atrae y la pregunta como llegar si mi prespuesto es ajustado'
+    img:'https://upload.wikimedia.org/wikipedia/commons/4/4b/Montaje_de_la_Ciudad_de_Buenos_Aires.png',
+    Title:'Buenos Aires',
+    Description:'Argentina es el destino'
 
 },
-
 {
     id:'1',
-    img:'https://m.media-amazon.com/images/I/81aDxpFVw8L._AC_UF1000,1000_QL80_.jpg',
-    Title:'Nunka',
-    Description:'Machu pichu es una de las maraviilas del mundo por el cual mucha gente se atrae y la pregunta como llegar si mi prespuesto es ajustado'
+    img:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Rome_Montage_2017.png/375px-Rome_Montage_2017.png',
+    Title:'Roma',
+    Description:'La grande italiaaaa'
 
 },
+{
+    id:'1',
+    img:'https://es.wikipedia.org/wiki/Archivo:Red_and_white_wine_12-2015.jpg',
+    Title:'Vinos',
+    Description:'Mini Guia sobre como aprender de vino, elegir, abrir y beberlo'
+
+},
+
 
 ];
--- src/fotos.js
+++ src/fotos.js
@@ -7,9 +7,9 @@ function Fotos() {
   return (
     <div>
       <Menu />
-<h1>Machu Pichu</h1>
- {/* Aqui Fotos*/} 
- <h1>Cuenca, Ecuador</h1>
+<h3>Machu Pichu</h3>
+
+ <h3>Cuenca, Ecuador</h3>
  <Footer />
 
     </div>
