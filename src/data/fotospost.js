5b9bd9114393fa9c65c009ca171469ceef02ae5f
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-04-23 18:53:12 +0100

    add

---
+++ src/data/fotospost.js
@@ -0,0 +1,11 @@
+export const Fotos = [
+    {
+        id:'1',
+        img:'https://m.media-amazon.com/images/I/81aDxpFVw8L._AC_UF1000,1000_QL80_.jpg',
+        Title:'Nunka',
+        Description:'Nunka es una website hippie sobre viajes y experiencias atraves de esta website'
+    
+    },
+    
+    
+    ];
