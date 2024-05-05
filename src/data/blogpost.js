f259d57805da44a66a5e26f7eae5a071234406bb
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-05-05 11:25:26 +0100

    fixingblog

--- src/data/blogpost.js
+++ src/data/blogpost.js
@@ -1,25 +1,26 @@
 
-
 export const Blogs = [
     {
         id:'1',
         img:'https://m.media-amazon.com/images/I/81aDxpFVw8L._AC_UF1000,1000_QL80_.jpg',
         Title:'Nunka',
         Description:'Nunka es una website hippie sobre viajes y experiencias atraves de esta website',
-       Link:'Nunka.md'
+       Link:'https://en.wikipedia.org/wiki/Elon_Musk'
     },
 {
     id:'1',
     img:'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Collage_Cuenca.png/800px-Collage_Cuenca.png',
     Title:'Cuenca',
-    Description:'De las mejores ciudades del ecuador'
+    Description:'De las mejores ciudades del ecuador' ,
+    Link:'https://en.wikipedia.org/wiki/Peter_Thiel'
 
 },
 {
     id:'1',
     img:'https://upload.wikimedia.org/wikipedia/commons/4/4b/Montaje_de_la_Ciudad_de_Buenos_Aires.png',
     Title:'Buenos Aires',
-    Description:'Argentina es el destino'
+    Description:'Argentina es el destino',
+    Link:'https://en.wikipedia.org/wiki/Aaron_Swartz'
 
 },
 {
