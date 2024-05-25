cc0eec10379ef8046c005f3b533e7a25c3c91118
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-05-25 19:31:54 +0100

    add

--- src/data/fotospost.js
+++ src/data/fotospost.js
@@ -1,14 +1,41 @@
 export const FotosData = [
     {
         id:'1',
-        imgs: [
-            require('../Imagenes/imgMachuPichu/foto2.png'),
-      
-        ],
-        Title: 'Machu Pichu',
-       
-    
+        img:'https://m.media-amazon.com/images/I/81aDxpFVw8L._AC_UF1000,1000_QL80_.jpg',
+        Title:'Machu Pichu',
+        Description:'Nunka es una website hippie sobre viajes y experiencias atraves de esta website',
+       Link:'https://en.wikipedia.org/wiki/Elon_Musk'
     },
+{
+    id:'1',
+    img:'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Collage_Cuenca.png/800px-Collage_Cuenca.png',
+    Title:'New york',
+    Description:'De las mejores ciudades del ecuador' ,
+    Link:'https://en.wikipedia.org/wiki/Peter_Thiel'
+
+},
+{
+    id:'1',
+    img:'https://upload.wikimedia.org/wikipedia/commons/4/4b/Montaje_de_la_Ciudad_de_Buenos_Aires.png',
+    Title:'Cuenca',
+    Description:'Argentina es el destino',
+    Link:'https://en.wikipedia.org/wiki/Aaron_Swartz'
+
+},
+{
+    id:'1',
+    img:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Rome_Montage_2017.png/375px-Rome_Montage_2017.png',
+    Title:'Galapagos',
+    Description:'La grande italiaaaa'
+
+},
+{
+    id:'1',
+    img:'https://es.wikipedia.org/wiki/Archivo:Red_and_white_wine_12-2015.jpg',
+    Title:'Kayak',
+    Description:'Mini Guia sobre como aprender de vino, elegir, abrir y beberlo'
+
+},
     
     
     ];
--- src/fotos.js
+++ src/fotos.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { Menu } from "./Menu";
-import { Blogs } from "../src/data/blogpost"; // Cambiado a Blogs
-import Renderpost from "./renderpost";
+import { FotosData } from "../src/data/fotospost"; // Cambiado a Blogs
+import {Render }from "./fotosrender";
 import { Footer } from "./footer";
 
 
-function Fotos() {
+function Fotos(props) {
   return (
     <div>
       <Menu /> 
       {/* Aquí escribir blogs en en markdown y unir a esta página*/} 
       <section className="bloglist"> 
-        {Blogs.map((blog, index) => ( 
-          <Renderpost 
+        {FotosData.map((FotosData, index) => ( 
+          <Render
             key={index} 
-            img={blog.img} 
-            Title={blog.Title} 
-            Description={blog.Description}
-            Link={blog.Link} 
+            img={FotosData.img} 
+            Title={FotosData.Title} 
+            Description={FotosData.Description}
+            Link={FotosData.Link} 
           />
         ))} 
       </section>
